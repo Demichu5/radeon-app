2539eeb5b6a086d4734947e29de13a8e52a1ca65
Michał <121185944+Demichu5@users.noreply.github.com>
2025-04-03 00:06:56 +0200

    Add files via upload

---
+++ color.py
@@ -0,0 +1,28 @@
+import pyray as pr
+
+BACKGROUND1 = pr.Color(29, 32, 33, 255)
+
+GRAY1 = pr.Color(40, 40, 40, 255)
+GRAY2 = pr.Color(60, 56, 54, 255)
+GRAY3 = pr.Color(80, 73, 69, 255)
+
+RED1 = pr.Color(204, 35, 29, 255)
+RED2 = pr.Color(251, 72, 52, 255)
+
+ORANGE1 = pr.Color(214, 94, 14, 255)
+ORANGE2 = pr.Color(254, 128, 25, 255)
+
+PURPLE1 = pr.Color(177, 98, 134, 255)
+PURPLE2 = pr.Color(211, 134, 155, 255)
+
+CYAN1 = pr.Color(104, 157, 106, 255)
+CYAN2 = pr.Color(142, 192, 124, 255)
+
+GREEN1 = pr.Color(152, 152, 26, 255)
+GREEN2 = pr.Color(184, 187, 38, 255)
+
+BLUE1 = pr.Color(69, 133, 136, 255)
+BLUE2 = pr.Color(131, 165, 152, 255)
+
+YELLOW1 = pr.Color(215, 154, 33, 255)
+YELLOW2 = pr.Color(250, 189, 47, 255)
